775d01a6e8cb31f3833f7bbe73a938d3c11fb8ec
mehediemon007 <mehediemon7@gmail.com>
2022-04-10 16:32:43 +0600

    fix

---
+++ src/data/data.js
@@ -0,0 +1,18 @@
+export const data = {
+    "products":[
+        {   id:0,
+            thumbnail:"black-stripe-shirt.jpg",
+            slug:"blck-stripe-shirt",
+            images:[
+                "red-stripe-shirt.jpg"
+            ],
+            name: "Premium Cotton Black Striped Full Shirt",
+            price:1299,
+            size:["S","M","L","XL","XXL"],
+            color:["black","blue","oragne"],
+            tag:"New",
+            discount: 100,
+            discountType: "Percentage",
+        },
+    ]
+}
