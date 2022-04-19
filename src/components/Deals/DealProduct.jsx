8f166de327f10c1010308c62d26dc25c2a52e3a9
mehediemon007 <mehediemon7@gmail.com>
2022-04-19 14:23:21 +0600

    fix

--- src/components/Deals/DealProduct.jsx
+++ src/components/Deals/DealProduct.jsx
@@ -8,18 +8,17 @@ const DealProduct = ({product}) => {
 
     return (
         <>
-            <div className="single-product">
+            <div className="single-product align-items-center">
                 <div className="product-image">
                     <Link to="/product-details"><img src={`images/products/${product.thumbnail}`} alt={product.alt}/></Link>
                     <span className='tag'>{product.discount}%</span>
                 </div>
-                <div className="product-content p-3">
+                <div className="product-content px-3">
                     <Link to="/product-details"><h5>{product.name}</h5></Link>
                     <p className='price'>&#2547; {product.price - ( product.price * product.discount ) / 100 }<del>&#2547; {product.price}</del></p>
-                    <p className='p-des'>{product.des}</p>
                     <div className="sale-count">
                         <div className="count-left">
-                            <h4>Hurry Up!</h4>
+                            <h4> Hurry Up!</h4>
                             <p>Deal Ends In</p>
                         </div>
                         <div className="sale-counter">
