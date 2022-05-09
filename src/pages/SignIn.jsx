af6982944e26f3530b571ca78ebf1948cdf1b577
mehediemon007 <mehediemon7@gmail.com>
2022-05-09 11:20:35 +0600

    signIn

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -7,30 +7,38 @@ const SignIn = () => {
     return (
         <>
             <Header/>
-            <main className='fs-main-content'>
+            <main className='fs-main-content py-4'>
                 <div className="container">
-                    <h3 className='text-center'>Sign In</h3>
-                    <div className="sign-form">
-                        <form action="">
-                            <div className="single-input">
-                                <label htmlFor="email">Email</label>
-                                <input type="email" id='email' placeholder='E-mail Address'/>
-                                <i class="fa-solid fa-envelope"></i>
+                    <h3 className='sign-head'>Sign In</h3>
+                    <div className='sign-form-wpr'>
+                        <div className="sign-form">
+                            <div className="social-sign-links">
+                                <a href="https://www.facebook.com/" className='social-sign'><i class="fa-brands fa-facebook"></i> Sign In With Facebook</a>
+                                <a href="https://www.facebook.com/" className='social-sign gmail-sign'><i class="fa-brands fa-google-plus-square"></i> Sign In With Gmail</a>
+                                <p> <span>........</span> Or, <Link to="/">sign in</Link> with your email<span> ........</span> </p>
                             </div>
-                            <div className="single-input position-relative">
-                                <label htmlFor="password">Password</label>
-                                <input type="password" id='password' placeholder='Password'/>
-                                <i class="fa-solid fa-lock"></i>
-                            </div>
-                            <div className="sign-action">
-                                <div className="signed-in">
-                                    <input type="checkbox" id='signedIn'/>
-                                    <label htmlFor="sigedIn">Keep Me Signed In</label>
+                            <form action="">
+                                <div className="single-input position-relative mb-3">
+                                    <label htmlFor="email">Email</label>
+                                    <input type="email" id='email' placeholder='E-mail Address'/>
+                                    <i class="fa-solid fa-envelope"></i>
                                 </div>
-                                <Link to="/">Foregt Password?</Link>
-                            </div>
-                            <button type='submit'>Sign In</button>
-                        </form>
+                                <div className="single-input position-relative mb-3">
+                                    <label htmlFor="password">Password</label>
+                                    <input type="password" id='password' placeholder='Password'/>
+                                    <i class="fa-solid fa-lock"></i>
+                                </div>
+                                <div className="sign-action">
+                                    <div className="signed-in">
+                                        <input type="checkbox" id='signedIn'/>
+                                        <label htmlFor="sigedIn">Keep Me Signed In</label>
+                                    </div>
+                                    <Link to="/">Foregt Password?</Link>
+                                </div>
+                                <button type='submit' className='sign-btn'>Sign In</button>
+                                <p className='text-center'>New Member? <Link to="/sign-up">Sign Up</Link></p>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </main>
