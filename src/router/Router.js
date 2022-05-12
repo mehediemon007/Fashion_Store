c07f90dd78b2df97538aab7ff323e59984034f16
mehediemon007 <mehediemon7@gmail.com>
2022-05-12 15:03:11 +0600

    scroll

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Routes, Route} from "react-router-dom";
+import React,{useLayoutEffect} from 'react';
+import {Routes, Route, useLocation} from "react-router-dom";
 import Category from '../pages/Category';
 import Home from '../pages/Home';
 import ProductDetails from '../pages/ProductDetails';
@@ -13,22 +13,32 @@ import Orders from '../pages/Orders';
 import OrderTrack from '../pages/OrderTrack';
 import SingleOrder from '../pages/SIngleOrder';
 
+const Wrapper = ({children}) => {
+    const location = useLocation();
+    useLayoutEffect(() => {
+      document.documentElement.scrollTo(0, 0);
+    }, [location.pathname]);
+    return children
+} 
+
 const routes = () => {
     return (
-        <Routes>
-            <Route exact path='/' element={<Home/>}/>
-            <Route exact path='/product_details/:product_id' element={<ProductDetails/>}/>
-            <Route exact path='/category/:category_name' element={<Category/>}/>
-            <Route exact path='/wishlist' element={<WishList/>}/>
-            <Route exact path='/shopcart' element={<ShopCart/>}/>
-            <Route exact path='/compare-products' element={<Compare/>}/>
-            <Route exact path='/sign-in' element={<SignIn/>}/>
-            <Route exact path='/sign-up' element={<SignUp/>}/>
-            <Route exact path='/chat' element={<SellerChat/>}/>
-            <Route exact path='/orders' element={<Orders/>}/>
-            <Route exact path='/orders/:orderId' element={<SingleOrder/>}/>
-            <Route exact path='/orders/:orderId/track' element={<OrderTrack/>}/>
-        </Routes>
+        <Wrapper>
+            <Routes>
+                <Route exact path='/' element={<Home/>}/>
+                <Route exact path='/product_details/:product_id' element={<ProductDetails/>}/>
+                <Route exact path='/category/:category_name' element={<Category/>}/>
+                <Route exact path='/wishlist' element={<WishList/>}/>
+                <Route exact path='/shopcart' element={<ShopCart/>}/>
+                <Route exact path='/compare-products' element={<Compare/>}/>
+                <Route exact path='/sign-in' element={<SignIn/>}/>
+                <Route exact path='/sign-up' element={<SignUp/>}/>
+                <Route exact path='/chat' element={<SellerChat/>}/>
+                <Route exact path='/orders' element={<Orders/>}/>
+                <Route exact path='/orders/:orderId' element={<SingleOrder/>}/>
+                <Route exact path='/orders/:orderId/track' element={<OrderTrack/>}/>
+            </Routes>
+        </Wrapper>
     )
 }
 
